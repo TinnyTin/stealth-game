399876198c365b1162e23b9fc7aa59b068519a66
Justin Wu <jwu811@gatech.edu>
2024-02-21 14:17:50 -0800

    create mesh display

--- Assets/Scripts/FieldOfView.cs
+++ Assets/Scripts/FieldOfView.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 
 public class FieldOfView : MonoBehaviour
@@ -8,6 +9,8 @@ public class FieldOfView : MonoBehaviour
     public float viewRadius;
     [Range(0,360)]
     public float viewAngle;
+    public float meshResolution = 1;
+    public MeshFilter viewMeshFilter;
 
     public LayerMask targetMask;
     public LayerMask obstacleMask;
@@ -15,9 +18,16 @@ public class FieldOfView : MonoBehaviour
     [HideInInspector]
     public List<Transform> visibleTargets = new List<Transform>();
 
+    // private
+    private Mesh viewMesh;
+
     // TODO remove this secion and add in fixedupdate...?
     private void Start()
     {
+        viewMesh = new Mesh();
+        viewMesh.name = "View Mesh";
+        viewMeshFilter.mesh = viewMesh;
+
         StartCoroutine("FindTargetsWithDelay", .2f);
     }
     IEnumerator FindTargetsWithDelay(float delay)
@@ -28,6 +38,13 @@ public class FieldOfView : MonoBehaviour
             FindVisibleTargets();
         }
     }
+
+    private void Update()
+    {
+        DrawFieldOfView();
+    }
+    
+    //  Find targets
     void FindVisibleTargets()
     {
         visibleTargets.Clear();
@@ -51,6 +68,8 @@ public class FieldOfView : MonoBehaviour
         }
     }
 
+    // convert Angle into vector3 direction
+    // angleIsGlobal indicates whether we use global or local coordinates
     public Vector3 DirFromAngle(float angleInDegrees, bool angleIsGlobal)
     {
         if (!angleIsGlobal)
@@ -58,5 +77,72 @@ public class FieldOfView : MonoBehaviour
             angleInDegrees += transform.eulerAngles.y;
         }
         return new Vector3(Mathf.Sin(angleInDegrees * Mathf.Deg2Rad),0,Mathf.Cos(angleInDegrees*Mathf.Deg2Rad));
+    }
+
+    void DrawFieldOfView()
+    {
+        int stepCount = Mathf.RoundToInt(viewAngle * meshResolution);
+        float stepAngleSize = viewAngle / stepCount;
+        List<Vector3> viewPoints = new List<Vector3>();
+        for (int i = 0; i <= stepCount; i++)
+        {
+            float angle = transform.eulerAngles.y - viewAngle / 2 + stepAngleSize * i;
+            ViewCastInfo newViewCast = ViewCast(angle);
+            viewPoints.Add(newViewCast.point);
+        }
+
+        int vertexCount = viewPoints.Count + 1; // origin vertex + viewCast points
+        Vector3[] vertices = new Vector3[vertexCount];
+        int[] triangles = new int[(vertexCount - 2) * 3]; // triangles formed = total vertices - 2
+
+        vertices[0] = Vector3.zero; // origin of local space
+        // triangle vertex indices
+        for (int i = 0; i < vertexCount - 1; i++)
+        {
+            vertices[i + 1] = transform.InverseTransformPoint(viewPoints[i]); // save in vertices
+            if (i < vertexCount - 2)
+            {
+                triangles[i * 3] = 0;
+                triangles[i * 3 + 1] = i + 1;
+                triangles[i * 3 + 2] = i + 2;
+            }
+        }
+
+        viewMesh.Clear();
+        viewMesh.vertices = vertices;
+        viewMesh.triangles = triangles;
+        viewMesh.RecalculateNormals();
+    }
+
+    // check cast using viewRadius and angle hits any obstacle. return info
+    ViewCastInfo ViewCast(float globalAngle)
+    {
+        Vector3 dir = DirFromAngle(globalAngle, true);
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, dir, out hit, viewRadius, obstacleMask))
+        {
+            return new ViewCastInfo(true,hit.point,hit.distance,globalAngle);
+        }
+        else
+        {
+            return new ViewCastInfo(false, transform.position + dir * viewRadius, viewRadius, globalAngle);
+        }
+    }
+    
+    // 
+    public struct ViewCastInfo
+    {
+        public bool hit;
+        public Vector3 point;
+        public float dist;
+        public float angle;
+
+        public ViewCastInfo(bool _hit, Vector3 _point, float _dist, float _angle)
+        {
+            hit = _hit;
+            point = _point;
+            dist = _dist;
+            angle = _angle;
+        }
     }
 }
