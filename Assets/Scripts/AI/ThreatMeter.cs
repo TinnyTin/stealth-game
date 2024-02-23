7be2189722e3aa0e17710a694a0790c33e46d1e1
Justin Wu <jwu811@gatech.edu>
2024-02-23 00:52:32 -0800

    created first ThreatMeter script

---
+++ Assets/Scripts/AI/ThreatMeter.cs
@@ -0,0 +1,102 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+
+[RequireComponent(typeof(AI))]
+public class ThreatMeter : MonoBehaviour
+{
+    public Slider threatBar;
+    public float minThreatMeter = 0;
+    public float maxThreatMeter = 100;
+    [Tooltip("Rate of increase in threat on sight")]
+    public float sightThreatRate = 10;
+    [Tooltip("Multiplier on anything that increases threat")]
+    public float NeuroticMultiplier = 1;
+    [Tooltip("Rate in threat per second")]
+    public float DegredationRate = 10;
+
+    [Header("Current threat level")]
+    public float threatLevel = 0;
+    public int BehaviorIdx;
+
+    [Header("Behaviors")]
+    public List<ThreatThreshold> ThreatBehaviorList;
+
+    private AI ai;
+    private void Start()
+    {
+        ai = GetComponent<AI>();
+        // invoke first behavior. usually idle
+        alignBehaviorType();
+        ThreatBehaviorList[BehaviorIdx].onThreatReach.Invoke();
+    }
+
+    private void FixedUpdate()
+    {
+        changeThreat(-(DegredationRate * Time.deltaTime));
+    }
+
+    public void onSightThreat(Transform t, float interval)
+    {
+        ai.lastThreat = new Vector3(t.position.x, t.position.y, t.position.z);
+        changeThreat(sightThreatRate * interval * NeuroticMultiplier);
+
+    }
+    public void changeThreat(float val)
+    {
+        if (val > 0)
+        {
+            threatLevel = Mathf.Clamp(threatLevel + val*NeuroticMultiplier, minThreatMeter, maxThreatMeter);
+        }
+        else
+        {
+            threatLevel = Mathf.Clamp(threatLevel + val, minThreatMeter, maxThreatMeter);
+        }
+        
+        onThreatChange();
+    }
+
+    private void onThreatChange()
+    {
+        // check if you have reached a new max.
+        alignBehaviorType();
+        // update threatBar
+        threatBar.value = threatLevel / maxThreatMeter;
+
+    }
+
+    private void alignBehaviorType()
+    {
+        int lastBhvrIdx = BehaviorIdx;
+        // check there is a higher behavior state to reach, and that the AI's threat has exceeded it
+        while ((BehaviorIdx + 1 < ThreatBehaviorList.Count) && (threatLevel > ThreatBehaviorList[BehaviorIdx].max))
+        {
+            BehaviorIdx++; // change to higher behavior
+        }
+        // check if threat degradation moves us to lower behavior state
+        while ((BehaviorIdx - 1 >= 0) && (threatLevel <= ThreatBehaviorList[BehaviorIdx].min))
+        {
+            BehaviorIdx--;
+        }
+        if (lastBhvrIdx != BehaviorIdx)
+            ThreatBehaviorList[BehaviorIdx].onThreatReach.Invoke(); // invoke behavior
+    }
+
+    
+
+
+    [System.Serializable]
+    public struct ThreatThreshold
+    {
+        public string name;
+        public float min;
+        public float max;
+        public int priority;
+        public UnityEvent onThreatReach;
+    }
+
+}
+
