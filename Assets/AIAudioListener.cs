0e4fb51f65fd83264270f3213a1b0c11e267f797
Justin Wu <jwu811@gatech.edu>
2024-03-03 20:47:20 -0800

    AI Sound Event hooked up with increase to threatmeter just based on being within radius range.

---
+++ Assets/AIAudioListener.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(ThreatMeter))]
+public class AIAudioListener : MonoBehaviour
+{
+    public float radiusListenable = 10f;
+    public float baseThreatPerSound = 50f;
+    public float weightThreatOfSound = 1f;
+    private ThreatMeter tm;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        tm = GetComponent<ThreatMeter>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void onSound3DStringInGameCreated(Component component, AudioClip sfx, Vector3 position, string str)
+    {
+        if (Vector3.Distance(position,this.transform.position) <= radiusListenable)
+        {
+            float val = baseThreatPerSound * weightThreatOfSound;
+            tm.changeThreat(val);
+        }
+    }
+}
