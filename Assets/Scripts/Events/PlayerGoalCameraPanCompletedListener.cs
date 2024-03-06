745e593bb26a2ccd97b09512a0ec7a2a2b6e6b40
Tom Burridge <tom@vorsprungsuspension.com>
2024-03-06 11:56:28 -0800

    Add player goal camera pan events and listeners

---
+++ Assets/Scripts/Events/PlayerGoalCameraPanCompletedListener.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+/*
+ * CS6457 Attributions
+ * Tiny Brain
+ * Original Author:     Tom
+ * Contributors:
+ *
+ * External
+ * Source Credit:       https://www.gamedev.lu/s/EventSystem_v10.zip
+ *                      https://www.youtube.com/watch?v=7_dyDmF0Ktw&ab_channel=ThisisGameDev
+ */
+
+[AddComponentMenu("Player Goal Camera Pan Started Listener")]
+public class PlayerGoalCameraPanCompletedListener : GameEventListenerBase, IEventListener
+{
+    [System.Serializable]
+    public class PlayerGoalCameraCompletedEvent : UnityEvent<Component> { }
+
+    [Header("Response Method To Invoke:")]
+    [Tooltip("Response to invoke when Event with GameData is raised.")]
+    public PlayerGoalCameraCompletedEvent response;
+
+    public void OnEventRaised(Component sender)
+    {
+        response.Invoke(sender);
+    }
+}
