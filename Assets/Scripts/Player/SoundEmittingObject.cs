41cacda79eea5c1c93aadd98dbc4c3af15be91cb
Justin Wu <jwu811@gatech.edu>
2024-03-09 16:13:11 -0800

    Balance AI Guard listening to sound
    Listen Radius 50 -> 15
    Threat Increase 50?? -> 15 (investigate)

--- Assets/Scripts/Player/SoundEmittingObject.cs
+++ Assets/Scripts/Player/SoundEmittingObject.cs
@@ -6,22 +6,28 @@ using UnityEngine;
 
 public class SoundEmittingObject : MonoBehaviour
 {
-  public AudioClip AudioClipCollision;
-  public GameEvent eventToRaise;
+    public AudioClip AudioClipCollision;
+    public GameEvent eventToRaise;
+    public float impulseThreshold = 5f;
 
-  // Start is called before the first frame update
-  void Start()
-  {
-    if(AudioClipCollision == null)
+    // Start is called before the first frame update
+    void Start()
     {
-      Debug.LogError("SoundEmittingObject: No SoundEmittingObject AudioClip");
+        if (AudioClipCollision == null)
+        {
+            Debug.LogError("SoundEmittingObject: No SoundEmittingObject AudioClip");
+        }
     }
-  }
 
-  // TODO: make this on fall, and vary sounds
-  public void OnCollisionEnter()
-  {
-    if(AudioClipCollision != null)
-      eventToRaise.Raise(AudioClipCollision, transform.position, "SoundThreat");
-  }
+    // TODO: make this on fall, and vary sounds
+    public void OnCollisionEnter(Collision c)
+    {
+        if (c.impulse.magnitude > impulseThreshold)
+        {
+            if (AudioClipCollision != null)
+                eventToRaise.Raise(AudioClipCollision, transform.position, "SoundThreat");
+        }
+
+
+    }
 }
