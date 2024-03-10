7a877520d7af7e05fd14365363abb2c7e94fa084
Martin Lee <mlee807@gatech.edu>
2024-03-09 22:04:08 -0800

    add mission summary menu navigation

--- Assets/Scripts/UI/MissionSummary.cs
+++ Assets/Scripts/UI/MissionSummary.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class MissionSummary : MonoBehaviour
 {
     // Temporary referencing structure until SO's are setup.
@@ -10,6 +10,7 @@ public class MissionSummary : MonoBehaviour
     public GameObject missionSummaryCamera;
     public GameObject hudCanvas;
     public GameObject missionSummaryCanvas;
+    public GameObject selectableObject;
 
     // Perform mission complete procedure
     public void missionComplete()
@@ -21,6 +22,7 @@ public class MissionSummary : MonoBehaviour
             mainCamera.SetActive(false);
             hudCanvas.SetActive(false);
             missionSummaryCanvas.SetActive(true);
+            selectableObject.GetComponent<Button>().Select();
         }
     }
 }
