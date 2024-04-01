c07158f472c777950612621fd5a090c3a537d682
Tom Burridge <tom@vorsprungsuspension.com>
2024-03-31 23:00:57 -0700

    Update TriggerActivateGameObject.cs
    Added null reference fix for empty array elements in GO array.
    
    Added ability to play audio clip on trigger activated.

--- Assets/Scripts/Gameplay/TriggerActivateGameObject.cs
+++ Assets/Scripts/Gameplay/TriggerActivateGameObject.cs
@@ -1,11 +1,21 @@
-
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+
+/*
+ * CS6457 Attributions
+ * Tiny Brain
+ * Original Author:     Justin Wu
+ * Contributors:        Tom
+ * Description: 
+ * External Source Credit:
+ *
+ */
 
 public class TriggerActivateGameObject : MonoBehaviour
 {
     public GameObject[] GOs;
+
+    public GameEvent AudioChannel;
+    public AudioClip AudioClip;
 
     bool isEnabled = false;
 
@@ -15,11 +25,14 @@ public class TriggerActivateGameObject : MonoBehaviour
         {
             foreach (GameObject GO in GOs)
             {
-                GO.SetActive(true);
+                if (GO != null) 
+                    GO.SetActive(true);
             }
+
+            if (AudioChannel != null)
+                AudioChannel.Raise(AudioClip, this.transform.position, AudioSourceParams.Default);
+            
             isEnabled = true;
         }
-
-
     }
 }
