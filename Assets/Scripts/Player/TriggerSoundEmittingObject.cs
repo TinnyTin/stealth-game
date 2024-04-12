5d590d33c32243c63580e28c441f4aff3d966ea2
Justin Wu <jwu811@gatech.edu>
2024-04-12 16:50:42 -0700

    Create broken glass prefab for library room

---
+++ Assets/Scripts/Player/TriggerSoundEmittingObject.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+
+/*
+ * CS6457 Attributions
+ * Tiny Brain
+ * Original Author:     Jeesoo
+ * Contributors:        
+ */
+
+public class TriggerSoundEmittingObject : MonoBehaviour
+{
+    [Header("Audio")]
+    public AudioClip AudioClipCollision;
+    public float delayToNextSound = 1f;
+
+    [Header("Events")]
+    public GameEvent soundEventToRaise;
+    public GameEvent soundThreatEvent;
+    public float threatWeight = 1f;
+
+    private float nextSound = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (AudioClipCollision == null)
+        {
+            Debug.LogError("SoundEmittingObject: No SoundEmittingObject AudioClip");
+        }
+    }
+
+    // TODO: make this on fall, and vary sounds
+    public void OnCollisionEnter(Collision c)
+    {
+        // make sure we can't spam the sound with infinite collisions per second
+        if (Time.time > nextSound)
+        {
+            // reset nextSound time
+            nextSound = Time.time + delayToNextSound;
+
+            // play audio clip
+            if (AudioClipCollision != null)
+            {
+                // raise sound event
+                soundEventToRaise.Raise(AudioClipCollision, transform.position, AudioSourceParams.Default);
+                // raise threat increase event
+                soundThreatEvent.Raise(transform.position, threatWeight);
+            }
+        }
+    }
+}
